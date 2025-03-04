ff92438aa471661498695a4d3f926edbc6cfa8b1
Mizko Danylo <mizko2000y@gmail.com>
2025-03-04 03:25:23 +0200

    Refresh hero. Make animation and connection between animation

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -5,10 +5,31 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     [SerializeField] private float speed = 3f;
+    private Animator animator;
     private void FixedUpdate()
     {
         float x = Input.GetAxis("Horizontal") * speed * Time.fixedDeltaTime;
         float z = Input.GetAxis("Vertical") * speed * Time.fixedDeltaTime;
         transform.Translate(x,0,z);
+        animator = GetComponent<Animator>();
+        AnimController(x, z);
+
+    }
+
+    void AnimController(float x, float z)
+    {
+        if(z > 0)
+        {
+            if(Input.GetKey(KeyCode.LeftShift)) animator.SetInteger("Move", 2);
+            else animator.SetInteger("Move", 1);
+        }
+        else if(z < 0)
+        {
+            animator.SetInteger("Move", -1);
+        }
+        else
+        {
+            animator.SetInteger("Move", 0);
+        }
     }
 }
