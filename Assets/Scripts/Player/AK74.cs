d88e98f57b90d5a132e75a1aa8cc50d5e3240be8
Mizko Danylo <mizko2000y@gmail.com>
2025-03-05 01:30:01 +0200

    Add shoot and firerate. Add maps and change lights settings

--- Assets/Scripts/Player/AK74.cs
+++ Assets/Scripts/Player/AK74.cs
@@ -3,23 +3,43 @@ using UnityEngine;
 public class AK74 : MonoBehaviour
 {
     [SerializeField] private float rayLenght = 50f;
+    [SerializeField] private float damage = 10f;
+    [SerializeField] float fireRate = 0.2f;
+
+    [SerializeField] ParticleSystem shootEffect;
+    [SerializeField] ParticleSystem hitEffect;
+    [SerializeField] AudioSource shootAudio;
+
+    [SerializeField] LayerMask ignoreLayer;
+
+
+    private float nextTimeToShoot = 0f;
 
     
     
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) Shoot();
+        if (Input.GetMouseButton(0) && Time.time >= nextTimeToShoot)
+        {
+            Shoot();
+            nextTimeToShoot = Time.time + fireRate;
+        }
+
     }
 
     void Shoot()
     {
+        shootAudio.Play();
+        shootEffect.Play();
         RaycastHit hit;
-        
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, rayLenght))
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, rayLenght, ~ignoreLayer))
         {
-            Debug.Log("Shoot");
-            if (hit.collider.gameObject.tag == "Mark") Debug.Log("Mark");
+            
+            if (hit.collider.gameObject.tag == "Mark") hit.collider.GetComponent<Target>().TakeDamage(damage);
+            Debug.Log("Hit: " + hit.collider.gameObject.name);
+            GameObject particle = Instantiate(hitEffect.gameObject, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(particle, 0.5f);
         }
-        
+
     }
 }
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float health = 50f;
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0) Die();
+    }
+
+    void Die()
+    {
+        gameObject.SetActive(false);
+    }
+}
